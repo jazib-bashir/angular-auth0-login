74a6e5613b6115aeebb0f2de84a6ac6b43bdd8b5
Dev Teknuk <dev@teknuk.com>
2019-06-24 14:57:12 +0500

    constants - added callback url for auth0

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -3,12 +3,14 @@ import { InjectionToken } from '@angular/core';
 export interface ApplicationConfig {
   auth0ClientID: string;
   auth0Domain: string;
+  auth0CallBack: string;
 }
 
 // Configuration values for our app
 export const MY_CONFIG: ApplicationConfig = {
   auth0ClientID: 'f1d7GIUsCxCltnfKVzH8yLkK3tk5EvIH',
-  auth0Domain: 'hotel-booking-test.auth0.com'
+  auth0Domain: 'hotel-booking-test.auth0.com',
+  auth0CallBack: 'http://localhost:4200/callback'
 };
 
 // Create a config token to avoid naming conflicts
